bba70fb20f417c626e41ec5a499d10b43098bbf5
yahelcohen01 <yahelcohen01@gmail.com>
2025-10-05 15:43:44 +0300

    fix > innovation section

--- app/components/innovation-section/innovation-section.tsx
+++ app/components/innovation-section/innovation-section.tsx
@@ -1,19 +1,30 @@
+import Image from "next/image";
+
 export const InnovationSection = () => {
   return (
     <section
-      className="relative w-full py-16 mt-16 px-4 md:px-16 lg:px-24 xl:px-32"
+      className="relative w-full py-16 mt-16 px-4 md:px-16 lg:px-24 xl:px-32 scroll-mt-30"
       id="innovation"
     >
-      <img
-        src="/assets/innovation-text.png"
-        alt="Innovation-text"
-        className="w-full object-cover h-max bg-transparent"
-      />
-      <img
-        src="/assets/innovation-processor.png"
-        alt="Innovation"
-        className="w-full object-cover h-max bg-transparent mt-8"
-      />
+      <div className="w-full">
+        <Image
+          src="/assets/innovation-text.png"
+          alt="Innovation-text"
+          width={1200}
+          height={400}
+          className="w-full h-auto object-contain"
+          priority
+        />
+      </div>
+      <div className="w-full">
+        <Image
+          src="/assets/innovation-processor.png"
+          alt="Innovation"
+          width={1200}
+          height={600}
+          className="w-full h-auto object-contain"
+        />
+      </div>
     </section>
   );
 };
